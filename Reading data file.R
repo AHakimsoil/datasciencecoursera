dc1fed50112a85a55d5f15bdb0471e4fa23bf622
Azizul Hakim <hakim861202@gmail.com>
2025-05-23 09:14:21 +0200

    update

---
+++ Reading data file.R
@@ -0,0 +1,13 @@
+
+read.table()
+read.csv()
+readLines()
+source()
+install.packages("dget")
+library(dget)
+unserialize()
+load()
+
+read.table(file(),header = , sep = , colClasses = )
+
+data = read.table("foo.txt")
---
+++ Readinglargedata.R
@@ -0,0 +1,4 @@
+
+  
+read.table("datatable.txt",nrows = 100)
+classes = sapply(simplify = )
