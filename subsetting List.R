3384d485ad3726728330c8f1ad48af6fe6f9a51a
Azizul Hakim <hakim861202@gmail.com>
2025-05-26 17:31:34 +0200

    update

---
+++ subsetting List.R
@@ -0,0 +1,8 @@
+#subsetting Lists
+x=list(foo=1:4, bar=0.6)
+x[1]
+x[[1]]
+x$bar
+x[["bar"]]
+x["bar"]
+
---
+++ subsetting.R
@@ -0,0 +1,9 @@
+#subsetting
+x = c("a", "b", "c", "d", "a")
+
+x[1]
+x[1:4]
+x[x>"a"]
+u = x>"a"
+u
+x[u]
